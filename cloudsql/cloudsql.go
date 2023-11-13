cd8e9704b3953bfa48540cc6141861c4fbfc6180
Lindsay <koalasoftwareconsulting@gmail.com>
2023-11-13 00:29:55 -0500

    Fix dockerfile and spacing

--- cloudsql/cloudsql.go
+++ cloudsql/cloudsql.go
@@ -1,48 +1,48 @@
 package cloudsql
 
 import (
-        "database/sql"
-        "fmt"
-        "log"
-        "os"
+	"database/sql"
+	"fmt"
+	"log"
+	"os"
 
-        // Note: If connecting using the App Engine Flex Go runtime, use
-        // "github.com/jackc/pgx/stdlib" instead, since v5 requires
-        // Go modules which are not supported by App Engine Flex.
-        _ "github.com/jackc/pgx/v5/stdlib"
+	// Note: If connecting using the App Engine Flex Go runtime, use
+	// "github.com/jackc/pgx/stdlib" instead, since v5 requires
+	// Go modules which are not supported by App Engine Flex.
+	_ "github.com/jackc/pgx/v5/stdlib"
 )
 
 // connectUnixSocket initializes a Unix socket connection pool for
 // a Cloud SQL instance of Postgres.
 func ConnectUnixSocket() (*sql.DB, error) {
-        mustGetenv := func(k string) string {
-                v := os.Getenv(k)
-                if v == "" {
-                        log.Fatalf("Fatal Error in connect_unix.go: %s environment variable not set.\n", k)
-                }
-                return v
-        }
-        // Note: Saving credentials in environment variables is convenient, but not
-        // secure - consider a more secure solution such as
-        // Cloud Secret Manager (https://cloud.google.com/secret-manager) to help
-        // keep secrets safe.
-        var (
-                dbUser         = mustGetenv("DB_USER")              // e.g. 'my-db-user'
-                dbPwd          = mustGetenv("DB_PASS")              // e.g. 'my-db-password'
-                unixSocketPath = mustGetenv("INSTANCE_UNIX_SOCKET") // e.g. '/cloudsql/project:region:instance'
-                dbName         = mustGetenv("DB_NAME")              // e.g. 'my-database'
-        )
+	mustGetenv := func(k string) string {
+		v := os.Getenv(k)
+		if v == "" {
+			log.Fatalf("Fatal Error in connect_unix.go: %s environment variable not set.\n", k)
+		}
+		return v
+	}
+	// Note: Saving credentials in environment variables is convenient, but not
+	// secure - consider a more secure solution such as
+	// Cloud Secret Manager (https://cloud.google.com/secret-manager) to help
+	// keep secrets safe.
+	var (
+		dbUser         = mustGetenv("DB_USER")              // e.g. 'my-db-user'
+		dbPwd          = mustGetenv("DB_PASS")              // e.g. 'my-db-password'
+		unixSocketPath = mustGetenv("INSTANCE_UNIX_SOCKET") // e.g. '/cloudsql/project:region:instance'
+		dbName         = mustGetenv("DB_NAME")              // e.g. 'my-database'
+	)
 
-        dbURI := fmt.Sprintf("user=%s password=%s database=%s host=%s",
-                dbUser, dbPwd, dbName, unixSocketPath)
+	dbURI := fmt.Sprintf("user=%s password=%s database=%s host=%s",
+		dbUser, dbPwd, dbName, unixSocketPath)
 
-        // dbPool is the pool of database connections.
-        dbPool, err := sql.Open("pgx", dbURI)
-        if err != nil {
-                return nil, fmt.Errorf("sql.Open: %w", err)
-        }
+	// dbPool is the pool of database connections.
+	dbPool, err := sql.Open("pgx", dbURI)
+	if err != nil {
+		return nil, fmt.Errorf("sql.Open: %w", err)
+	}
 
-        // ...
+	// ...
 
-        return dbPool, nil
+	return dbPool, nil
 }
